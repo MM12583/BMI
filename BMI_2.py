4a8446effd35e3e536ba5d26270848c2b89a34b4
MM12583 <lordarcher12345@gmail.com>
2020-10-18 16:42:18 +0800

    add

--- BMI_2.py
+++ BMI_2.py
@@ -93,9 +93,9 @@ def sort(data_list) :
     weight_s = []
     for d in data_list :
         name = d[0]
-        tall = d[1]
-        weight = d[2]
-        BMI_n = d[3]
+        tall = float(d[1])
+        weight =float(d[2])
+        BMI_n = float(d[3])
         BMI_s.append([name,BMI_n])
         Tall_s.append([name,tall])
         weight_s.append([name,weight])
@@ -104,6 +104,47 @@ def sort(data_list) :
     Tall_s.sort(key = lambda x:x[1])
     weight_s.sort(key = lambda x:x[1])
     return BMI_s,Tall_s,weight_s  # 多回傳
+
+# 圓餅圖
+def matplotlib(data_list) :
+    import matplotlib.pyplot as plt
+    plt.rcParams['font.sans-serif'] = ['MingLiU'] # 字型
+
+    light = 0
+    normal = 0
+    over = 0
+    fat = 0
+    fat_n = 0
+    fat_s = 0
+    for d in data_list :
+        status = d[4]
+        if status == '過輕' :
+            light += 1
+        if status == '正常' :
+            normal += 1
+        if status == '過重' :
+            over += 1
+        if status == '輕度肥胖' :
+            fat += 1
+        if status == '中度肥胖' :
+            fat_n += 1
+        if status == '重度肥胖' :
+            fat_s += 1
+            
+    light_p = round((100/len(data_list) * light),2)
+    normal_p = round((100/len(data_list)*normal),2)
+    over_p = round((100/len(data_list)*over),2)
+    fat_p = round((100/len(data_list)*fat),2)
+    fat_n_p = round((100/len(data_list)*fat_n),2)
+    fat_s_p = round((100/len(data_list)*fat_s),2)
+
+    labels = '過輕', '正常', '過重', '輕度肥胖', '中度肥胖', '重度肥胖'
+    sizes = [light_p, normal_p, over_p, fat_p, fat_n_p, fat_s_p]
+    explode = (0, 0, 0, 0, 0, 0) # 設置分離的距離，0表示不分離
+    plt.pie(sizes, explode = explode, labels = labels, autopct = '%1.1f%%',
+    shadow = True, startangle = 90)
+    plt.axis('equal') # 畫出正圓形
+    plt.show()
 
 # 主程式
 def main() :
@@ -117,5 +158,6 @@ def main() :
     BMI_s = sort(data_list)[0]
     Tall_s = sort(data_list)[1]
     weight_s = sort(data_list)[2]
+    matplotlib(data_list)
 
 main()
